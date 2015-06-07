9cab6687cf2da1e0d1ae20c3e688e5423d98eacd
Ivan <igaglioti@actiaitalia.com>
2015-06-07 16:29:52 +0200

    Initial commit

---
+++ app/src/main/java/org/gagi/nativedrawerapp/fragments/ContentFragment.java
@@ -0,0 +1,32 @@
+package org.gagi.nativedrawerapp.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.gagi.nativedrawerapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ContentFragment extends Fragment {
+
+
+    public ContentFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.content_fragment,container,false);
+        return v;
+    }
+
+
+}
